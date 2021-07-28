097bdf3e9d531133b867cae071bf2b5c977abaad
gabriel <gabrielcossy1996@gmail.com>
2021-07-28 15:16:13 -0300

    Se agrego el MVC de CategoriaProductos

---
+++ Curso.Web/Controllers/CategoriaProductosController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Curso.DataAccess.Models;
+
+namespace Curso.Web.Controllers
+{
+    public class CategoriaProductosController : Controller
+    {
+        private readonly EjAppContext _context;
+
+        public CategoriaProductosController(EjAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CategoriaProductoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.CategoriaProducto.ToListAsync());
+        }
+
+        // GET: CategoriaProductoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoriaProducto = await _context.CategoriaProducto
+                .FirstOrDefaultAsync(m => m.IdCategoriaProducto == id);
+            if (categoriaProducto == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoriaProducto);
+        }
+
+        // GET: CategoriaProductoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CategoriaProductoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdCategoriaProducto,Nombre,Categoria")] CategoriaProducto categoriaProducto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categoriaProducto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoriaProducto);
+        }
+
+        // GET: CategoriaProductoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoriaProducto = await _context.CategoriaProducto.FindAsync(id);
+            if (categoriaProducto == null)
+            {
+                return NotFound();
+            }
+            return View(categoriaProducto);
+        }
+
+        // POST: CategoriaProductoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdCategoriaProducto,Nombre,Categoria")] CategoriaProducto categoriaProducto)
+        {
+            if (id != categoriaProducto.IdCategoriaProducto)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categoriaProducto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoriaProductoExists(categoriaProducto.IdCategoriaProducto))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoriaProducto);
+        }
+
+        // GET: CategoriaProductoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoriaProducto = await _context.CategoriaProducto
+                .FirstOrDefaultAsync(m => m.IdCategoriaProducto == id);
+            if (categoriaProducto == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoriaProducto);
+        }
+
+        // POST: CategoriaProductoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var categoriaProducto = await _context.CategoriaProducto.FindAsync(id);
+            _context.CategoriaProducto.Remove(categoriaProducto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoriaProductoExists(int id)
+        {
+            return _context.CategoriaProducto.Any(e => e.IdCategoriaProducto == id);
+        }
+    }
+}
