94d0dff1439461ee4a7d7bc9bcec6b20850c82d4
Pablo Guajardo <pablosguajardo@yahoo.com.ar>
2021-07-29 13:40:52 -0300

    add example

--- Curso.ConsoleApp/Program.cs
+++ Curso.ConsoleApp/Program.cs
@@ -6,7 +6,34 @@ namespace Curso.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("process init");
+
+
+            /*
+
+              public static string GetFile(string fileName)
+                {
+                    return File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "\\Templates\\" + fileName , System.Text.Encoding.UTF8);
+                }
+
+                public static string GetFileJson(string fileName)
+                {
+                    return File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "\\Templates\\" + fileName , System.Text.Encoding.UTF8);
+                    //var algo = JsonConvert.DeserializeObject<Objeto>(string);
+                }
+
+                public static string GetAllFiles(string dirName)
+                {
+                    foreach (var file in Directory.EnumerateFiles(dirName, "*.json", SearchOption.AllDirectories))
+                    {
+                        Console.WriteLine(" - " + file);
+               
+                    }
+                    return null;
+                }
+
+            */
+            Console.WriteLine("process end");
         }
     }
 }
