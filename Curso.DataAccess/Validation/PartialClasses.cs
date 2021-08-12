8625f287f4c53dd00537e0a2ad087b9f26b8156c
Pablo Guajardo <pablosguajardo@yahoo.com.ar>
2021-08-12 14:22:45 -0300

    add metadata

---
+++ Curso.DataAccess/Validation/PartialClasses.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using static Curso.DataAccess.Validations.Metadata;
+
+
+//Importante es el namespace:
+namespace Curso.DataAccess.Models
+{
+
+    [ModelMetadataType(typeof(PersonasMetadata))]
+    public partial class Personas
+    {
+    }
+
+  
+
+
+
+    [ModelMetadataType(typeof(AspNetUsersMetadata))]
+    public partial class AspNetUsers
+    {
+    }
+}
