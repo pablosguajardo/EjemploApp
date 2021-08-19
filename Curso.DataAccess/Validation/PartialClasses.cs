3d919577b68a2787a3a95080d795c7c77906203c
MatiasGiordano <mj.giordano99@hotmail.com>
2021-08-19 10:24:06 -0300

    "Se agrega validación de Ventas"

--- Curso.DataAccess/Validation/PartialClasses.cs
+++ Curso.DataAccess/Validation/PartialClasses.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Curso.DataAccess.Validations;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
